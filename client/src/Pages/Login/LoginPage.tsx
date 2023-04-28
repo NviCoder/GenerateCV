d8f43228b5cafab2df2186cdd9409883adfd3049
NviCoder <NviCoder@example.com>
2023-04-28 11:44:51 +0300

    Login page template

---
+++ client/src/Pages/Login/LoginPage.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  Container,
+  Title,
+  Form,
+  Input,
+  SubmitButton,
+  ForgotPassword,
+  SignupLink,
+  DocumentImg,
+  SSOButton,
+  GoogleButtonIcon
+} from "./LoginStyles";
+
+const IMAGE_SOURCE = "https://cdn-icons-png.flaticon.com/512/3135/3135686.png";
+
+const LoginPage = () => {
+  return (
+    <Container>
+      <DocumentImg src={IMAGE_SOURCE} />
+      <Title>Welcome Back</Title>
+      <Form>
+        <Input type="text" placeholder="Username" />
+        <Input type="password" placeholder="Password" />
+        <SSOButton>
+          <GoogleButtonIcon
+            src={"https://cdn-icons-png.flaticon.com/512/300/300221.png"}
+          />
+          Sign in with Google
+        </SSOButton>
+        <SubmitButton>Sign In</SubmitButton>
+        <ForgotPassword>Forgot Password?</ForgotPassword>
+      </Form>
+      <SignupLink>Don't have an account? Sign up</SignupLink>
+    </Container>
+  );
+};
+
+export default LoginPage;
