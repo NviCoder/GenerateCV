b680506599ac6577ff7a7427ea1b77ea9c9cbda1
Nvi Coder <44750316+NviCoder@users.noreply.github.com>
2023-05-14 07:05:07 +0000

    Finish configured server with cors

--- client/src/server/index.ts
+++ client/src/server/index.ts
@@ -6,11 +6,9 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    preflightDisallowedRedirect: true
-  }));
+    credentials: true,
+    origin: true}
+    ));
 
 app.use(api)
 
